34d0989db2b7e18c84c457c960b26a320153a5d2
Skyler Spaeth <skylerspaeth.scc@me.com>
2020-05-29 01:17:07 -0500

    successfully parsing DOM form input to object

--- public/js/form.js
+++ public/js/form.js
@@ -1,20 +1,24 @@
-let formData = JSON.stringify($("#newSomething").serializeArray());
-// let formData = $('#newSomething').serializeJSON();
-// let formData = $("#newSomething").serializeArray();
-// console.log("form data:" + formData);
-let submitHandler = (e) => {
-    e.preventDefault();
-    console.log("bp1");
-    console.log(formData);
-    $.ajax({
-        type: "POST",
-        url: "/submit?type=newRift",
-        data: formData,
-        success: function () {
-            console.log('form data posted:' + formData);
-        },
-        dataType: "json",
-        contentType: "application/json"
-    });
-    return false;
+// let formData = JSON.stringify($("#newSomething").serializeArray());
+// // let formData = $('#newSomething').serializeJSON();
+// // let formData = $("#newSomething").serializeArray();
+// // console.log("form data:" + formData);
+// let submitHandler = (e) => {
+//     e.preventDefault();
+//     console.log("bp1");
+//     console.log(formData);
+//     $.ajax({
+//         type: "POST",
+//         url: "/submit?type=newRift",
+//         data: formData,
+//         success: function () {
+//             console.log('form data posted:' + formData);
+//         },
+//         dataType: "json",
+//         contentType: "application/json"
+//     });
+//     return false;
+// }
+
+function yeet() {
+    console.log($('form').serializeJSON());
 }
