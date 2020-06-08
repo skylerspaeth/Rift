76d9f429e9949edac84b1b32f6c30c8af48fc322
skylerspaeth <skylerspaeth.scc@me.com>
2020-06-07 23:20:32 -0500

    Still working on resolving the returned promise

--- models/interface.js
+++ models/interface.js
@@ -7,26 +7,28 @@ async function run() {
     const { riftSchema } = require('./rift.model.js');
     const Rift = mongoose.model('Rift', riftSchema);
 
-    module.exports.allRifts = await function () { Rift.find({}, (err, results) => err ? err : results).lean().exec() };
+    module.exports.allRifts = await function () { Rift.find({}, (err, results) => err ? err : results).lean().exec().then(() => { var retRifts = Promise.resolve(allRifts); console.log(retRifts) }) }
+}
 
-    module.exports.riftCrud = {
-        create: function (object) {
-            Rift.create(object);
-        },
-        read: function (riftName) {
-            Rift.find({ name: riftName }, (err, result) => {
-                if (err) { return err; }
-                else { return result; }
-            });
-        },
-        update: function () { },
-        delete: function () { },
-        forEach: async function () {
-            await Rift.find({}, (err, results) => {
-                // ret = results;
-                // console.log(ret);
-                return results;
-            }).exec()
-        }
+module.exports.riftCrud = {
+    create: function (object) {
+        Rift.create(object);
+    },
+    read: function (riftName) {
+        Rift.find({ name: riftName }, (err, result) => {
+            if (err) { return err; }
+            else { return result; }
+        });
+    },
+    update: function () { },
+    delete: function () { },
+    forEach: async function () {
+        await Rift.find({}, (err, results) => {
+            // ret = results;
+            // console.log(ret);
+            return results;
+
+        }).exec()
     }
+}
 }
